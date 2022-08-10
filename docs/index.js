3d5a1871ebc5ca1092afb491ff76a9a54df2f4f7
GijsVj <71672125+GijsVj@users.noreply.github.com>
2022-08-10 21:46:24 +0200

    Big parts of Front-end done with API functionality

--- docs/index.js
+++ docs/index.js
@@ -1,34 +1,50 @@
-function getPhone(){
-    fetch(`https://api-mobilespecs.azharimm.site/v2/samsung_galaxy_s20_fe_5g-10377`)
-    .then(response => response.json())
-    .then(data => {
-        console.log("succes",data);
-            
-        let containerInfo = document.getElementById("container");
-        let htmlString = 
-                `<h2>${data.data.brand}: ${data.data.phone_name}</h2>
-                <div class="flex-row">
-                    <img class="result-image" src="${data.data.thumbnail}" alt="result">
-                    <div class="aspects">
-                        <p>The aspects that you found important:</p>
-                        <ul>
-                            <li>Long battery</li>
-                            <li>Good camera</li>
-                            <li>Nice screen</li>
-                            <li>Price below €800</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <a href="index.html"><button id="nextButton">Retake test</button></a>
-                    </div>
-                </div>`;
-                
-        
-        containerInfo.insertAdjacentHTML('beforeend', htmlString);
+//Inspiration from pokémon exercise
 
+let list = [];
+let phone = [];
+
+function getData(){
+    //get phone list
+    fetch('https://api-mobilespecs.azharimm.site/v2/top-by-fans')
+    .then(response => {
+        return response.json();
+    }).then(data => {
+        list = data.data.phones;
+        //Loop list of phones
+        for(let element of list){
+            fetch(element.detail).then(response => {
+                return response.json();
+             }).then(data => {
+                 phone.push(data);
+             })
+        }
     });
 }
 
-getPhone();
+window.onload = function(){
+
+    getData();
+    setTimeout(buildList, 3000);
+
+    function buildList(){
+        let html = '';
+        // Ordering of my list
+        phone.sort(function(a,b) {
+            return a.id - b.id;
+        });
 
 
+        for(let p of phone){
+            html += `<div class="itemsContainer"> <img src="${p.data.thumbnail}" alt="" width="100%">
+            <div class="flexRow">
+                <div class="flexCol">
+                    <p class="brand">${p.data.brand}</p>
+                    <p class="type">${p.data.phone_name}</p>
+                </div>
+                <input type="checkbox" id="liked">
+            </div>
+            </div>`
+        }
+        document.getElementById('list').innerHTML = html;
+    }
+}
